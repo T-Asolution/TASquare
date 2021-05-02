20f7d761973274f8c731de569e62815d6dafc2d6
T.Endo <stargate.taku@gmail.com>
2021-05-02 20:22:33 +0900

    new project

---
+++ TASquare/src/main/java/com/tasolution/tasquare/TaSquareApplication.java
@@ -0,0 +1,13 @@
+package com.tasolution.tasquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaSquareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaSquareApplication.class, args);
+	}
+
+}
---
+++ TASquare/src/test/java/com/tasolution/tasquare/TaSquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasolution.tasquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaSquareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
