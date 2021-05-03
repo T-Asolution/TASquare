8e8998d43fc3072bbae5212c80ffe37dbdea665a
T.Endo <stargate.taku@gmail.com>
2021-05-04 00:08:46 +0900

    ログイン画面、プロジェクト一覧画面作成

--- TASquare/src/main/java/com/tasolution/tasquare/controller/HeloController.java
+++ TASquare/src/main/java/com/tasolution/tasquare/controller/HeloController.java
@@ -2,12 +2,20 @@ package com.tasolution.tasquare.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HeloController {
 
 	@RequestMapping("/")
-	public String index() {
-		return "Hello Spring-Boot World!!";
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("login");
+		return mav;
+	}
+	
+	@RequestMapping("/project")
+	public ModelAndView project(ModelAndView mav) {
+		mav.setViewName("project");
+		return mav;
 	}
 }
