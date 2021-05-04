cf93c3ba4609a4ad30f35080b7f4b7c2a23fed87
T.Endo <stargate.taku@gmail.com>
2021-05-04 23:22:12 +0900

    Controllerクラス名変更

---
+++ TASquare/src/main/java/com/tasolution/tasquare/controller/mainController.java
@@ -0,0 +1,21 @@
+package com.tasolution.tasquare.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class mainController {
+
+	@RequestMapping("/")
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("login");
+		return mav;
+	}
+	
+	@RequestMapping("/project")
+	public ModelAndView project(ModelAndView mav) {
+		mav.setViewName("project");
+		return mav;
+	}
+}
